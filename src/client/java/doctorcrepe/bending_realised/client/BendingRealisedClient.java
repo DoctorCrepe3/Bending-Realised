010db1b1dbda4081a201a31cb3885667448a4676
kg4021 <kg4021@hw.ac.uk>
2026-05-21 20:21:08 +0100

    swapped to fabric for better documentation

---
+++ src/client/java/doctorcrepe/bending_realised/client/BendingRealisedClient.java
@@ -0,0 +1,10 @@
+package doctorcrepe.bending_realised.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BendingRealisedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
